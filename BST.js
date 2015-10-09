34be4ea98f729f3e4f9a12b53a08febf0b378c8f
Alex Tareshawty <atareshawty@gmail.com>
2015-10-08 22:35:41 -0400

    Initialized repository. Insertion and print implemented

---
+++ BST.js
@@ -0,0 +1,82 @@
+;
+/*
+This impl of binary search tree takes data that can be compared with < >, etc
+Constructor defaults to an empty tree
+
+*/
+function BST(headData) {
+  if (typeof headData === 'undefined') {
+    console.log('You didn\'t give my any data! This won\'t work');
+    return;
+  } else {
+    this.head = new Node(headData);
+  }
+}
+
+function Node(data) {
+  this.data = data;
+  this.left = null;
+  this.right = null;
+}
+
+BST.prototype.inOrderPrint = function() {
+  if (this.head != null) {
+    var headStore = this.head;
+    this.head = this.head.left;
+    this.inOrderPrint();
+    console.log(headStore.data);
+    this.head = headStore.right;
+    this.inOrderPrint();
+    this.head = headStore;
+  }
+};
+
+BST.prototype.preOrderPrint = function() {
+  if (this.head != null) {
+    var headStore = this.head;
+    console.log(this.head.data);
+    this.head = this.head.left;
+    this.preOrderPrint();
+    this.head = headStore.right;
+    this.preOrderPrint();
+    this.head = headStore;
+  }
+};
+
+BST.prototype.postOrderPrint = function() {
+  if (this.head != null) {
+    var headStore = this.head;
+    this.head = this.head.left;
+    this.preOrderPrint();
+    this.head = headStore.right;
+    this.preOrderPrint();
+    this.head = headStore;
+    console.log(this.head.data);
+  }
+};
+
+BST.prototype.insert = function(data) {
+  if (typeof this.head.data != typeof data) console.log('Will only accept the same data type we started with');
+  var headStore = this.head;
+  if (this.head.data >= data) {
+    if (this.head.left != null) {
+      this.head = this.head.left;
+      this.insert(data);
+    } else {
+      this.head.left = new Node(data);
+    }
+  } else {
+    if (this.head.right != null) {
+      this.head = this.head.right;
+      this.insert(data);
+    } else {
+      this.head.right = new Node(data);
+    }
+  }
+
+  this.head = headStore;
+};
+
+
+
+module.exports = BST;
---
+++ main.js
@@ -0,0 +1,5 @@
+var bst = require("./BST.js");
+var myTree = new bst(15);
+myTree.insert(10);
+myTree.insert(12);
+myTree.insert(100);
